3c8d6e1333fb60be753e3d866a031d03dcbb7a9a
Coleman Sandefur <coleman.sandefur@gmail.com>
2021-06-12 00:34:49 -0500

    Moved handlers to dedicated functions

--- src/window.rs
+++ src/window.rs
@@ -4,7 +4,7 @@ use crate::click::Click;
 use crate::camera_transform::CameraTransform;
 
 use cairo::Context;
-use gdk::{EventButton, EventMask, ScrollDirection};
+use gdk::{EventMask, ScrollDirection};
 use glib::signal::Inhibit;
 use std::sync::{Arc, Mutex};
 use std::collections::VecDeque;
@@ -18,7 +18,7 @@ pub struct Window {
     drawing_area: Arc<DrawingArea>,
 
     quilt: Arc<Mutex<Quilt>>,
-    mouse_clicks: Arc<Mutex<VecDeque<EventButton>>>,
+    mouse_clicks: Arc<Mutex<VecDeque<gdk::EventButton>>>,
     frame_timing: Arc<Mutex<FrameTiming>>,
     zoom_amount: Arc<Mutex<f64>>,
     camera_transform: Arc<Mutex<CameraTransform>>,
@@ -26,6 +26,8 @@ pub struct Window {
 
 impl Window {
     pub fn new(application: &gtk::Application) -> Arc<Self> {
+        //create variables before putting it into a window
+        //just in case I want to use them later
         let window = Arc::new(gtk::ApplicationWindow::new(application));
         let drawing_area = Arc::new(Box::new(DrawingArea::new)());
         let quilt = Arc::new(Mutex::new(
@@ -46,7 +48,11 @@ impl Window {
             camera_transform: Arc::clone(&camera_transform),
         });
 
-        //s_clone is always moved into a closure
+        //
+        // Event handlers
+        //
+
+        //s_clone will always moved into a closure
         let s_clone = Arc::clone(&s);
         drawing_area.connect_draw(move |drawing_area, cr| {
             s_clone.draw(drawing_area, cr)
@@ -60,76 +66,20 @@ impl Window {
         
         let s_clone = Arc::clone(&s);
         window.add_events(EventMask::KEY_PRESS_MASK);
-        window.connect_key_press_event(move |_, event| {
-            let key = event.get_keyval();
-
-            if let Some(name) = key.name() {
-                let mut camera_transform = s_clone.camera_transform.lock().unwrap();
-
-                if name.eq("a") {
-                    camera_transform.start_move_left();
-                }
-
-                if name.eq("d") {
-                    camera_transform.start_move_right();
-                }
-
-                if name.eq("w") {
-                    camera_transform.start_move_up();
-                }
-
-                if name.eq("s") {
-                    camera_transform.start_move_down();
-                }
-            }
-
-            Inhibit(false)
+        window.connect_key_press_event(move |window, event| {
+            s_clone.on_key_press(window, event)
         });
 
         let s_clone = Arc::clone(&s);
         window.add_events(EventMask::KEY_RELEASE_MASK);
-        window.connect_key_release_event(move |_, event| {
-            let key = event.get_keyval();
-
-            if let Some(name) = key.name() {
-                let mut camera_transform = s_clone.camera_transform.lock().unwrap();
-
-                if name.eq("a") {
-                    camera_transform.stop_move_left();
-                }
-
-                if name.eq("d") {
-                    camera_transform.stop_move_right();
-                }
-
-                if name.eq("w") {
-                    camera_transform.stop_move_up();
-                }
-
-                if name.eq("s") {
-                    camera_transform.stop_move_down();
-                }
-            }
-
-            Inhibit(false)
+        window.connect_key_release_event(move |window, event| {
+            s_clone.on_key_release(window, event)
         });
 
         let s_clone = Arc::clone(&s);
         drawing_area.add_events(EventMask::SCROLL_MASK);
-        drawing_area.connect_scroll_event(move |_, event| {
-            
-            let mut camera_transform = s_clone.camera_transform.lock().unwrap();
-            if event.get_direction() == ScrollDirection::Up {
-                camera_transform.scale += 0.1;
-            }
-
-            if event.get_direction() == ScrollDirection::Down && camera_transform.scale > 0.1 {
-                camera_transform.scale -= 0.1;
-            }
-
-            println!("zoom: {}", camera_transform.scale);
-
-            Inhibit(false)
+        drawing_area.connect_scroll_event(move |drawing_area, event| {
+            s_clone.on_scroll(drawing_area, event)
         });
 
 
@@ -140,8 +90,38 @@ impl Window {
         s
     }
 
+    fn pre_draw(&self, _drawing_area: &DrawingArea, cr: &Context) {
+        let frame_timing = self.frame_timing.lock().unwrap();
+
+        //save the context before the transformation
+        //just in case we want to remove any transformations later
+        cr.save();
+
+        //random offset that I'll remove later
+        cr.translate(20.0, 20.0);
+
+        //will handle any necessary camera movements and apply them
+        self.handle_camera(cr, frame_timing.deref());
+        
+        //should handle clicks before draw
+        self.handle_clicks(cr);
+    }
+
     fn draw(&self, drawing_area: &DrawingArea, cr: &Context) -> Inhibit {
+        self.pre_draw(drawing_area, cr);
+
+        self.quilt.lock().unwrap()
+            .draw(cr);
+
+        self.post_draw(drawing_area, cr);
+
+        Inhibit(false)
+    }
+
+    fn post_draw(&self, drawing_area: &DrawingArea, cr: &Context) {
         let mut frame_timing = self.frame_timing.lock().unwrap();
+
+        cr.restore();
 
         //draw frame timing in top left corner
         cr.save();
@@ -153,32 +133,83 @@ impl Window {
 
         cr.restore();
 
-        //random offset that I'll remove later
-        cr.translate(20.0, 20.0);
-
-        //will handle any necessary camera movements and apply them
-        self.handle_camera(cr, frame_timing.deref());
-        
-        //should handle clicks before
-        self.handle_clicks(cr);
-
-        let quilt = self.quilt.lock().unwrap();
-        quilt.draw(cr);
-        drop(quilt);
-
+        //update the last recorded time we rendered a frame
         frame_timing.update_frame_time();
-
         drawing_area.queue_draw();
-
-        Inhibit(false)
     }
 
     //will add any clicks that drawing area recieves and add it to a queue for handle_clicks to use on next draw
-    fn on_click(&self, _drawing_area: &DrawingArea, event: &EventButton) -> Inhibit {
+    fn on_click(&self, _drawing_area: &DrawingArea, event: &gdk::EventButton) -> Inhibit {
         let mut mouse_clicks = self.mouse_clicks.lock().unwrap();
 
         mouse_clicks.push_back(event.to_owned());
         drop(mouse_clicks);
+
+        Inhibit(false)
+    }
+
+    fn on_scroll(&self, _drawing_area: &DrawingArea, event: &gdk::EventScroll) -> Inhibit {
+        let mut camera_transform = self.camera_transform.lock().unwrap();
+
+        if event.get_direction() == ScrollDirection::Up {
+            camera_transform.scale += 0.1;
+        }
+
+        if event.get_direction() == ScrollDirection::Down && camera_transform.scale > 0.1 {
+            camera_transform.scale -= 0.1;
+        }
+
+        Inhibit(false)
+    }
+
+    fn on_key_press(&self, _drawing_area: &gtk::ApplicationWindow, event: &gdk::EventKey) -> Inhibit {
+        let key = event.get_keyval();
+
+        if let Some(name) = key.name() {
+            let mut camera_transform = self.camera_transform.lock().unwrap();
+
+            if name.eq("a") {
+                camera_transform.start_move_left();
+            }
+
+            if name.eq("d") {
+                camera_transform.start_move_right();
+            }
+
+            if name.eq("w") {
+                camera_transform.start_move_up();
+            }
+
+            if name.eq("s") {
+                camera_transform.start_move_down();
+            }
+        }
+
+        Inhibit(false)
+    }
+
+    fn on_key_release(&self, _drawing_area: &gtk::ApplicationWindow, event: &gdk::EventKey) -> Inhibit {
+        let key = event.get_keyval();
+
+        if let Some(name) = key.name() {
+            let mut camera_transform = self.camera_transform.lock().unwrap();
+
+            if name.eq("a") {
+                camera_transform.stop_move_left();
+            }
+
+            if name.eq("d") {
+                camera_transform.stop_move_right();
+            }
+
+            if name.eq("w") {
+                camera_transform.stop_move_up();
+            }
+
+            if name.eq("s") {
+                camera_transform.stop_move_down();
+            }
+        }
 
         Inhibit(false)
     }
